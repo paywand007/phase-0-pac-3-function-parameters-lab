064ec8525544076d579d2ba6a69ad5420aaa65de
Paywand Mohammed Hussein <73426000+paywand007@users.noreply.github.com>
2022-07-31 18:06:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-function sayHelloTo(firstName = "User") {
-    console.log(`Hello, ${firstName}!`);
+function introduction(name = "Aki") {
+    return `Hi, my name is ${name}.`;
+}
+
+function introductionWithLanguage(name = "Aki", language = "Ember.js") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(
+    name = "Aki",
+    language = "JavaScript"
+) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
